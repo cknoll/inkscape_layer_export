c899bd781683cc74b6458793f111687edf1399f7
Carsten Knoll <carsten.knoll@posteo.de>
2018-03-30 22:23:35 +0200

    add support for "--end"-syntax + cleanup

--- svg_layer_export.py
+++ svg_layer_export.py
@@ -12,23 +12,28 @@ inkscape --file=mySVGinputFile.svg --export-area-drawing --without-gui --export-
 
 """
 
-
-from svglib.svglib import node_name, SvgRenderer
-import svglib
 from lxml import etree
 import os
 import sys
+import re
 
-from ipydex import IPS
+# TODO: implement node_name self
+from svglib.svglib import node_name
+
+# from ipydex import IPS
 
 
-IS_PREFIX="{http://www.inkscape.org/namespaces/inkscape}"
+IS_PREFIX = "{http://www.inkscape.org/namespaces/inkscape}"
 
-# source: https://graphicdesign.stackexchange.com/questions/5880/how-can-you-export-an-inkscape-svg-file-to-a-pdf-and-maintain-the-integrity-of-t
-inkscape_cmd_template = "inkscape --file={} --export-area-page --without-gui --export-pdf={}"
+
+inkscape_cmd_template = "inkscape --file={} --export-area-page " \
+                        "--without-gui --export-ignore-filters --export-pdf={}"
 
 
 def run_command(cmd, msg):
+    """
+    helper function: run command an give a meaningful error message
+    """
 
     returncode = os.system(cmd)
     if returncode == 0:
@@ -38,23 +43,31 @@ def run_command(cmd, msg):
 
 
 def islayer(svgnode):
+    """
+    determine if an xml node of an svg is a layer object
+    """
     if not node_name(svgnode) == "g":
         return False
     gmkey = "{}groupmode".format(IS_PREFIX)
-    gm = svgnode.attrib.get(gmkey, None)
+    grp_mode = svgnode.attrib.get(gmkey, None)
 
-    if gm == "layer":
+    if grp_mode == "layer":
         return True
 
     return False
 
+
 class Layer(object):
+    """
+    Inksacpe layer class
+    """
     maxframe = None
 
     def __init__(self, svg_node):
         self.svg_node = svg_node
         self._get_attributes()
-        self.frames = self._get_desired_frames()
+        self.frame_id_str = self._get_desired_frames_id_strings()
+        self.frames = None
 
     def __repr__(self):
         return "Layer. label='{}', attrib={}".format(self.label,
@@ -64,32 +77,62 @@ class Layer(object):
         self.style = self.svg_node.attrib.get("style")
         self.label = self.svg_node.attrib.get("{}label".format(IS_PREFIX))
 
-    def _get_desired_frames(self):
+    def _get_desired_frames_id_strings(self):
         """
-        assume the frame_number, where this layer should occour is encoded in the
-        layer-name like self.label == "some_string__[1, 2, 4, 7]"
+        assume, that the frame numbers, where this layer should be visble is
+        encoded in the layer-name like one of
+         - self.label == "some_string__[1, 2, 4, 7]"
+         - self.label == "some_string__[3--6]"
+         - self.label == "some_string__[3--end]"
         """
 
-        if not ( "__[" in self.label and self.label.endswith("]") ):
+        if not ("__[" in self.label and self.label.endswith("]")):
             # this layer is not desired in the result
-            return []
+            return ""
 
-        #IPS()
+        # IPS()
         idx = self.label.index("__[") + len("__[")
 
-        frame_str_list = self.label[idx:-1].split(",")
+        frame_id_str = self.label[idx:-1]
 
-        # todo: catch errors and produce meaningful message
-        frame_list = list(map(int, frame_str_list))
+        return frame_id_str
 
-        if frame_list == []:
-            return []
+    def gen_desired_frames(self):
+        """
+        generate desired frames (from string expressions like "3--end")
 
-        mf = max(frame_list)
-        if Layer.maxframe is None or mf > Layer.maxframe:
-            Layer.maxframe = mf
+        assume that maxframe has bien set
+        """
 
-        return frame_list
+        assert Layer.maxframe is not None
+
+        frame_id_str = self.frame_id_str.replace("end", "{}".format(Layer.maxframe + 1))
+
+        # now we only have expressions like "3--12" or "1, 2, 3"
+        # resolve the first one:
+        ranges = re.findall(r'\d+--\d+', frame_id_str)
+
+        self.frames = []
+
+        for r in ranges:
+            i1, i2 = map(int, r.split("--"))
+            lst = list(range(i1, i2 + 1))
+            frame_id_str = frame_id_str.replace(r, "")
+            frame_id_str = frame_id_str.replace(", ,", ",")
+            frame_id_str = frame_id_str.replace(",,", "")
+
+            self.frames.extend(lst)
+
+        # look for remaining regular sequences (# like 3, 4, 5)
+        frame_str_list = frame_id_str.split(",")
+
+        # TODO: catch errors and produce meaningful message
+        print(frame_str_list)
+        if not frame_str_list == ['']:
+            frame_list = list(map(int, frame_str_list))
+            frame_list.sort()
+
+            self.frames.extend(frame_list)
 
     def set_visibility(self, visibility_flag):
         """
@@ -101,9 +144,14 @@ class Layer(object):
         """
 
         style_str = self.svg_node.attrib.get("style")
+
+        if not style_str:
+            style_str = "display:none"
+
         # assume this is a ";"-separated list of ":"-separated key-value-pairs
 
         items = []
+
         for pair in style_str.split(";"):
             k, v = pair.split(":")
             items.append((k, v))
@@ -115,38 +163,46 @@ class Layer(object):
             style_dict["display"] = "none"
 
         # now convert back to string representation
-        new_style_str = ";".join(["{}:{}".format(k,v) for k,v in style_dict.items()])
+        new_style_str = ";".join(["{}:{}".format(k, v) for k, v in style_dict.items()])
 
         print("{}: {}  -->  {}".format(self.label, style_str, new_style_str))
 
         # change the actual svg-object
-        self.svg_node.attrib["style"]=new_style_str
-
-
-
+        self.svg_node.attrib["style"] = new_style_str
 
 
 def read_svg(path, **kwargs):
-    "Convert an SVG file to an RLG Drawing object."
-
-    # unzip .svgz file into .svg
-    unzipped = False
-    if isinstance(path, str) and os.path.splitext(path)[1].lower() == ".svgz":
-        data = gzip.GzipFile(path, "rb").read()
-        open(path[:-1], 'w').write(data)
-        path = path[:-1]
-        unzipped = True
+    """
+    Parse svg-Document
+    """
 
     # load SVG file
     parser = etree.XMLParser(remove_comments=True, recover=True)
     try:
         doc = etree.parse(path, parser=parser)
-        svg = doc.getroot()
+        svg_object = doc.getroot()
     except Exception as exc:
-        logger.error("Failed to load input file! (%s)" % exc)
+        print("Failed to load input file! (%s)" % exc)
         exit()
 
-    return svg
+    return svg_object
+
+
+def determine_max_frame(layers):
+
+    all_numbers = []
+
+    for lyr in layers:
+        all_numbers.extend(re.findall(r'\d+', lyr.frame_id_str))
+        print(lyr.label, "all_numbers:", all_numbers)
+
+    all_numbers = list(map(int, all_numbers))
+    all_numbers.sort()
+    print("all_numbers:", all_numbers, "..->", int(all_numbers[-1]))
+
+    # TODO: handle case of empty list
+    return int(all_numbers[-1])
+
 
 def layer_list(svg):
     all_elements_it = svg.getiterator()
@@ -155,8 +211,13 @@ def layer_list(svg):
     for elt in all_elements_it:
         if islayer(elt):
             lyr = Layer(elt)
-            print(lyr.label,":", lyr.style, lyr.frames)
+            print(lyr.label, ":", lyr.style, lyr.frames)
             layers.append(lyr)
+
+    Layer.maxframe = determine_max_frame(layers)
+
+    for lyr in layers:
+        lyr.gen_desired_frames()
 
     return layers
 
@@ -179,9 +240,8 @@ def render_layer_selections(layer_list, **kwargs):
     # iterate over frames (starting with 1, including maxframe)
     for framenbr in range(1, Layer.maxframe + 1):
         print("\n\n-- Frame: {}\n".format(framenbr))
-        visible_layers = []
-        invisible_layers = []
-
+        # visible_layers = []
+        # invisible_layers = []
 
         pdfbasename = svgbasename.replace(".svg", "-{:02d}.pdf".format(framenbr))
 
@@ -194,28 +254,20 @@ def render_layer_selections(layer_list, **kwargs):
             else:
                 layer.set_visibility(0)
 
-
         tmpsvgpath = "__tmp__.svg"
         with open(tmpsvgpath, "w") as svgfile:
             svgfile.write(etree.tostring(svg, encoding="utf8", pretty_print=True).decode("utf8"))
 
-        #IPS()
-        #exit()
-
         cmd = inkscape_cmd_template.format(tmpsvgpath, targetpath)
-        result = os.system(cmd)
         run_command(cmd, msg="{} written".format(targetpath))
 
         # TODO: delete tmpsvgpath
 
-
-    pdfwildcardpath = svgbasename.replace(".svg", "*.pdf")
-    pdfallpath = svgbasename.replace(".svg", "-all.pdf")
+    pdfwildcardpath = svgbasename.replace(".svg", "-*.pdf")
+    pdfallpath = svgbasename.replace(".svg", "_all.pdf")
     pdftkcommand = "pdftk {} cat output {}".format(pdfwildcardpath, pdfallpath)
 
     run_command(pdftkcommand, "overview generated")
-
-
 
 
 svg = read_svg(sys.argv[1])
